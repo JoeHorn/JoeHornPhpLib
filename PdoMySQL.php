911f555eb5dac4e7736cb7fad3da37a72f927a41
Joe Horn <joehorn@gmail.com>
2017-10-26 17:10:24 +0800

    Improvement
    - Supports lazy connection ( Do real connect before executing MySQL command )
    - Re-connect & re-try MySQL command if gotten unknown error or "MySQL server has gone away"

--- PdoMySQL.php
+++ PdoMySQL.php
@@ -2,215 +2,279 @@
 /*
  * MySQL class using PDO.
  *
- * @author	Joe Horn <joehorn@gmail.com>
- * @category	Class
- * @copyright	Copyright (c) 2013-2015, Joe Horn
- * @license	http://www.opensource.org/licenses/bsd-license.php The BSD License
+ * Major features :
+ *
+ *  - quote() supports array input
+ *  - Combine error information from PDO & PDOStatement classes
+ *  - Supports lazy connection ( Do real connect before executing MySQL command )
+ *  - Re-connect & re-try MySQL command if gotten unknown error or "MySQL server has gone away"
+ *
+ * Recent modified: 2017-10-26
+ * PHP version tested : 5.1 ~ 7.1
+ *
+ * @author      Joe Horn <joehorn@gmail.com>
+ * @category    Class
+ * @copyright   Copyright (c) 2013-2017, Joe Horn
+ * @license     http://www.opensource.org/licenses/bsd-license.php The BSD License
  */
-class PdoMySQL extends PDO {
-	/*
-	 * Error Information
-	 *
-	 * @access	private
-	 * @var		array	$ErrInfo
-	 */
-	private $ErrInfo = array('00000',null,null);
+if ( !class_exists('PdoMySQL') ) {
+    class PdoMySQL {
+        private $_dbName        = '';
+        private $_dbUsername    = 'root';
+        private $_dbPassword    = '';
+        private $_dbHost        = 'localhost';
+        private $_dbPort        = 3306;
 
-	/*
-	 * Constructor function
-	 *
-	 * @param	string	$db_name	Database name
-	 * @param	string	$db_username	Database username, default: 'root'
-	 * @param	string	$db_password	Database password, default: ''
-	 * @param	string	$charset	Database connection charset, default: 'UTF8'
-	 * @param	string	$db_host	Database server IP/hostname, default: 'localhost'
-	 * @param	integer	$db_port	Database server port, default: 3306
-	 * @param	boolean $persist	Create Database persistent connections, default: true
-	 */
-	function __construct (
-		$db_name,
-		$db_username = 'root',
-		$db_password = '',
-		$charset = 'UTF8',
-		$db_host = 'localhost',
-		$db_port = 3306,
-		$persist = true
-	) {
-		try {
-			parent::__construct(
-				"mysql:host=$db_host;port=$db_port;dbname=$db_name",
-				$db_username, $db_password,
-				array(
-					PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES $charset" ,
-					PDO::ATTR_PERSISTENT => $persist
-				)
-			);
-		} catch (Exception $e) {
-			throw $e;
-		}
-	}
+        private $_charset       = 'UTF8';
+        private $_persistent    = true;
+        private $_lazy          = false;
 
-	/*
-	 * Reset error information
-	 *
-	 * @access	private
-	 */
-	private function resetErrInfo() {
-		$this->ErrInfo = array('00000',null,null);
-	}
+        private $_conn          = null;
 
-	/*
-	 * Delete function
-	 *
-	 * @access	public
-	 * @param	string $sql		SQL statement
-	 * @return	int			Deleted rows count, -1 while error occurs
-	 */
-	public function delete ( $sql ) {
-		$this->resetErrInfo();
+        private $_errInfo       = array('00000',null,null);
 
-		$affectedRows = parent::exec($sql);
-		$this->ErrInfo = parent::errorInfo();
+        /*
+         * Constructor function
+         *
+         * @param    string     $db_name        Database name
+         * @param    string     $db_username    Database username, default: 'root'
+         * @param    string     $db_password    Database password, default: ''
+         * @param    string     $charset        Database connection charset, default: 'UTF8'
+         * @param    string     $db_host        Database server IP/hostname, default: 'localhost'
+         * @param    integer    $db_port        Database server port, default: 3306
+         * @param    boolean    $persist        Create Database persistent connections, default: true
+         */
+        function __construct (
+            $dbName,
+            $dbUsername = 'root',
+            $dbPassword = '',
+            $charset = 'UTF8',
+            $dbHost = 'localhost',
+            $dbPort = 3306,
+            $persistent = true,
+            $lazy = false
+        ) {
+            $this->_dbName      = $dbName;
+            $this->_dbUsername  = $dbUsername;
+            $this->_dbPassword  = $dbPassword;
+            $this->_dbHost      = $dbHost;
+            $this->_dbPort      = $dbPort;
 
-		if ( $affectedRows == false ) {
-			$affectedRows = -1;
-		}
-		return $affectedRows;
-	}
+            $this->_charset     = $charset;
+            $this->_persistent  = $persistent;
+            $this->_lazy        = $lazy;
 
-	/*
-	 * Retrieve error information
-	 *
-	 * @access	public
-	 * @return	array		An array with error information
-	 */
-	public function errorInfo() {
-		$myErrInfo = $this->ErrInfo;
+            if ( !$this->_lazy ) {
+                $this->connect();
+            }
+        }
 
-		if ( empty($myErrInfo[1]) && empty($myErrInfo[2]) ) {
-			return parent::errorInfo();
-		} else {
-			return $myErrInfo;
-		}
-	}
+        /*
+         * Magic method for calling PDO methods
+         */
+        public function __call ( $method , $params ) {
+            if ( $this->_lazy ) {
+                $this->connect();
+            }
 
-	/*
-	 * Execute SQL statement
-	 *
-	 * @access	public
-	 * @param	string		$sql		SQL statement
-	 * @param	array		$bindParams	An array with parameters
-	 * @return	PDOStatement			Executed PDOStatement
-	 */
-	public function execPrepared ( $sql , $bindParams ) {
-		$this->resetErrInfo();
+            if ( is_callable( array($this->_conn, $method), false) ) {
+                $result = call_user_func_array(
+                    array($this->_conn, $method),
+                    $params
+                );
+                $this->_errInfo = $this->_conn->errorInfo();
 
-		try {
-			$st = parent::prepare($sql);
-			if ( !$st ) {
-				$this->ErrInfo = parent::errorInfo();
-			} else {
-				$st->execute($bindParams);
-				$this->ErrInfo = $st->errorInfo();
-			}
-			return $st;
-		} catch (Exception $e) {
-			throw $e;
-		}
-	}
+                // Unknown error or "MySQL server has gone away" , re-connect & re-try
+                $retry = 0;
+                while ( 'HY000' == $this->_errInfo[0] && 10 > $retry ) {
+                    $this->connect(true);
+                    $result = call_user_func_array(
+                        array($this->_conn, $method),
+                        $params
+                    );
+                    $retry++;
+                }
 
-	/*
-	 * Get single row
-	 *
-	 * @access	public
-	 * @param	string $sql		SQL statement
-	 * @return	array			An array contains one data row
-	 */
-	public function getRow ( $sql , $fetchMode = PDO::FETCH_BOTH ) {
-		$this->resetErrInfo();
+                return $result;
+            } else {
+                error_log('[ERROR] ' . __CLASS__ . ' :' . " method ( $method ) not found!");
+                return null;
+            }
+        }
 
-		$st = parent::query($sql);
-		if ( !$st ) {
-			$this->ErrInfo = parent::errorInfo();
-			return array();
-		} else {
-			$row = $st->fetch($fetchMode);
-			$this->ErrInfo = $st->errorInfo();
-			return $row;
-		}
-	}
 
-	/*
-	 * Get multi rows
-	 *
-	 * @access	public
-	 * @param	string $sql		SQL statement
-	 * @return	array			An array contains data rows
-	 */
-	public function getRows ( $sql , $fetchMode = PDO::FETCH_BOTH ) {
-		$this->resetErrInfo();
+        /*
+         * Connect to MySQL ( for lazy connect )
+         *
+         * @access  private
+         */
+        private function connect ( $reConnect = false ) {
+            $this->_errInfo = array('00000',null,null);
 
-		$st = parent::query($sql);
-		if ( !$st ) {
-			$this->ErrInfo = parent::errorInfo();
-			return array();
-		} else {
-			$row = $st->fetchAll($fetchMode);
-			$this->ErrInfo = $st->errorInfo();
-			return $row;
-		}
-	}
+            if ( null == $this->_conn || $reConnect ) {
+                try {
+                    $this->_conn = new PDO(
+                        "mysql:host={$this->_dbHost};port={$this->_dbPort};dbname={$this->_dbName}",
+                        $this->_dbUsername, $this->_dbPassword,
+                        array(
+                            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES {$this->_charset}" ,
+                            PDO::ATTR_PERSISTENT => $this->_persistent
+                        )
+                    );
+                } catch (Exception $e) {
+                    throw $e;
+                }
+            }
+        }
 
-	/*
-	 * Insert function
-	 *
-	 * @access	public
-	 * @param	string $sql		SQL statement
-	 * @return	string			Last insert ID
-	 */
-	public function insert ( $sql ) {
-		$this->resetErrInfo();
+        /*
+         * Delete function
+         *
+         * @access  public
+         * @param   string  $sql    SQL statement
+         * @return  int             Deleted rows count, -1 while error occurs
+         */
+        public function delete ( $sql ) {
+            $affectedRows = $this->__call('exec', $sql);
 
-		$st = parent::query($sql);
-		if ( !$st ) {
-			$this->ErrInfo = parent::errorInfo();
-			return '';
-		} else {
-			$this->ErrInfo = $st->errorInfo();
-			return parent::lastInsertId();
-		}
-	}
+            if ( $affectedRows === false ) {
+                $this->_errInfo = $this->_conn->errorInfo();
+                $affectedRows = -1;
+            }
+            return $affectedRows;
+        }
 
-	/*
-	 * Extended quote function ( Support array to be put in WHERE ... IN ... clause )
-	 *
-	 * @access	public
-	 * @param	mixed $var		Variable to be quoted
-	 * @return	string			Quoted string
-	 */
-	public function quote ( $var , $parameterType = PDO::PARAM_STR ) {
-		$this->resetErrInfo();
+        /*
+         * Retrieve error information
+         *
+         * @access  public
+         * @return  array       An array with error information
+         */
+        public function errorInfo() {
+            $myErrInfo = $this->_errInfo;
 
-		if ( !is_array($var) ) {
-			return parent::quote($var, $parameterType);
-		} else {
-			foreach ( $var as $k => $v ) {
-				$tmpArr[$k] = parent::quote($v, $parameterType);
-			}
-			return implode(', ', $tmpArr);
-		}
-	}
+            if ( empty($myErrInfo[1]) && empty($myErrInfo[2]) ) {
+                return $this->_conn->errorInfo();
+            } else {
+                return $myErrInfo;
+            }
+        }
 
-	/*
-	 * Update function
-	 *
-	 * @access	public
-	 * @param	string $sql		SQL statement
-	 * @return	int			Affected rows count, -1 while error occurs
-	 */
-	public function update ( $sql ) {
-		$this->resetErrInfo();
 
-		return $this->delete($sql);
-	}
+        /*
+         * Execute SQL statement
+         *
+         * @access  public
+         * @param   string          $sql        SQL statement
+         * @param   array           $bindParams An array with parameters
+         * @return  PDOStatement                Executed PDOStatement
+         */
+        public function execPrepared ( $sql , $bindParams ) {
+            $st = $this->__call('prepare', $sql);
+            if ( !$st ) {
+                $this->_errInfo = $this->_conn->errorInfo();
+            } else {
+                $res = $st->execute($bindParams);
+                if ( false === $res ) {
+                    $this->_errInfo = $st->errorInfo();
+                }
+            }
+            return $st;
+        }
+
+        /*
+         * Get single row
+         *
+         * @access  public
+         * @param   string  $sql    SQL statement
+         * @return  array           An array contains one data row
+         */
+        public function getRow ( $sql , $fetchMode = PDO::FETCH_BOTH ) {
+            $st = $this->__call('query', $sql);
+            if ( !$st ) {
+                $this->_errInfo = $this->_conn->errorInfo();
+                $row = array();
+            } else {
+                $row = $st->fetch($fetchMode);
+                if ( $row === false ) {
+                    $row = array();
+                    $this->_errInfo = $st->errorInfo();
+                }
+            }
+            return $row;
+        }
+
+        /*
+         * Get multi rows
+         *
+         * @access  public
+         * @param   string  $sql    SQL statement
+         * @return  array           An array contains data rows
+         */
+        public function getRows ( $sql , $fetchMode = PDO::FETCH_BOTH ) {
+            $st = $this->__call('query', $sql);
+            if ( !$st ) {
+                $this->_errInfo = $this->_conn->errorInfo();
+                $row = array();
+            } else {
+                $row = $st->fetchAll($fetchMode);
+                if ( false === $row ) {
+                    $row = array();
+                    $this->_errInfo = $st->errorInfo();
+                }
+            }
+            return $row;
+        }
+
+        /*
+         * Insert function
+         *
+         * @access  public
+         * @param   string  $sql    SQL statement
+         * @return  string          Last insert ID
+         */
+        public function insert ( $sql ) {
+            $st = $this->__call('query', $sql);
+            if ( !$st ) {
+                $this->_errInfo = $this->_conn->errorInfo();
+                return '';
+            } else {
+                return $this->_conn->lastInsertId();
+            }
+        }
+
+        /*
+         * Extended quote function ( Support array to be put in WHERE ... IN ... clause )
+         *
+         * @access  public
+         * @param   mixed   $var    Variable to be quoted
+         * @return  string          Quoted string
+         */
+        public function quote ( $var , $parameterType = PDO::PARAM_STR ) {
+            if ( $this->_lazy ) {
+                $this->connect();
+            }
+
+            // PDO->quote() no need to reconnect , not using __call()
+            if ( !is_array($var) ) {
+                return $this->_conn->quote($var, $parameterType);
+            } else {
+                foreach ( $var as $k => $v ) {
+                    $tmpArr[$k] = $this->_conn->quote($v, $parameterType);
+                }
+                return implode(', ', $tmpArr);
+            }
+        }
+
+        /*
+         * Update function
+         *
+         * @access  public
+         * @param   string  $sql    SQL statement
+         * @return  int             Affected rows count, -1 while error occurs
+         */
+        public function update ( $sql ) {
+            return $this->delete($sql);
+        }
+    }
 }
